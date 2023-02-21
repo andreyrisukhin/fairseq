e8f072f98be1cdd6bbf7434f4084af2366cc7dd9
Andrey Risukhin <andrey.risukhin@outlook.com>
2023-02-21 14:40:55 -0800

    template generation works!

--- fairseq/modules/multihead_templates.py
+++ fairseq/modules/multihead_templates.py
@@ -5,28 +5,25 @@ from torch.nn.init import xavier_uniform_
 from torch.nn.init import constant_
 from torch import empty
 
+import numpy as np
+
 # Multihead Dense Synthesizer in Einsum
 class TemplatesManualMH(nn.Module):
     def __init__(self, in_dims, sentence_length, heads:int=1):             
-        ''' 
-        Einsum indicies and what they represent
-            b = batch size
-            d = in_dims
-            e = in_dims, distinct dimension from 'd' 
-            s = sentence length
-            t = sentence length, distinct dimension from 's' 
-            u = sentence length, distinct values from 's', 't' (needed in second linear projection)
-            h = attention heads
-
-        TODO best style: seq_len, batch, head, ...; this is due to gains by looping through tokens when decoding
         '''
+        MLP to learn the weights of templates for different input tokens.
+        '''
+        # All templates are the same, square size
+        # TODO Store templates as a tensor, n x n x (num templates)
+        # TODO multihead case?
+
         super(TemplatesManualMH, self).__init__() # ASK why not super().__init()__
 
         # Calculate dimension per head
         head_dim = in_dims // heads 
         assert (head_dim * heads == in_dims), "embed in_dims must be divisible by number of heads"
 
-        print(f'AR SynthDense Init')
+        print(f'AR Template Init')
         print(f'  head_dim: {head_dim}') # 64
         print(f'  in_dims: {in_dims}') # 512
         print(f'  sentence_len: {sentence_length}') # 512  # 2048 (b/c --max-tokens) did not pass the assertion # TODO ASK HAO, unless 2048 is max input and we project down to output 512? Feels unlikely, no longer seq2seq of same length, but ask
@@ -37,7 +34,7 @@ class TemplatesManualMH(nn.Module):
         self.in_dims = in_dims
         self.head_dim = head_dim
 
-        ''' Weights and Biases for Linear Layers '''
+        ''' Weights and Biases for Multilayer Perceptron (linear, relu/other activation, linear) '''
         self.w0 = Parameter(xavier_uniform_(empty(heads, in_dims, sentence_length,)))  # Linear 1 weights 
         self.b0 = Parameter(constant_(empty(sentence_length,), 0.0))  # Linear 1 bias
         self.w1 = Parameter(xavier_uniform_(empty(heads, sentence_length, sentence_length,))) # Lin 2 weights 
@@ -50,6 +47,64 @@ class TemplatesManualMH(nn.Module):
         self.value_b = Parameter(constant_(empty(head_dim,), 0.0)) # Need a bias vector for each attention head, stored here
 
         # print(f'Initialized SynthDenseEinsum. indims={in_dims}, seqlen={sentence_length}')
+
+        ''' Templates are fixed, not learnable. 
+        TODO ask: Size = sentence_length x sentence_length x in_dims, each? 
+        
+        Big Bird templates:
+        > Random attention, with 2 random elements selected per row
+        > Window attention, width=3 diagonal
+        > Global attention, g=2 (Rotated L to cover left and top sides)
+        '''
+        
+        # Random attention
+        
+        rng = np.random.default_rng()
+
+        ''' Takes length, returns torch square zero matrix with 2 elements in each row set to 1.'''
+        def template_random(n:int, r:int=2):
+            assert r <= n, f'Cannot have more inputs than allowed dimension'
+            t = np.zeros((n, n))
+            for row in t:
+                idxs = rng.choice(n, size=2)
+                for i in idxs:
+                    row[i] = 1
+            return torch.tensor(t)
+
+        ''' Takes dim, returns a square torch matrix with a diagonal of width. '''
+        def template_window(n:int, w:int=3):
+            assert w <= n, f'Cannot have more inputs than allowed dimension'
+            t = np.zeros((n, n))
+            for rid, row in enumerate(t):
+                # for i in range(w): #range(start=rid-(w//2), stop=rid+(w//2)):
+                #     if rid+i-(w//2) < n and rid+i+(w//2) >= 0: # OOB condition
+                #     # if i < n and i >= 0: # OOB condition
+                #         row[i] = 1
+                for i in range(rid-(w//2), rid+(w//2)+1, 1):
+                    if i < n and i >= 0: # OOB condition
+                        row[i] = 1
+
+            return torch.tensor(t)
+
+        def template_global(n:int, g:int=2):
+            assert g <= n, f'Cannot have more inputs than allowed dimension'
+            t = np.zeros((n, n))
+            global_line = np.ones((n,))
+            for i in range(g):
+                t[i] = global_line.copy() # Horizontal line of 1s
+                t.T[i] = global_line.copy() # Vertical line of 1s
+            return torch.tensor(t)
+
+        t1 = template_random(n=10)# sentence_length)
+        t2 = template_window(n=10)# sentence_length)
+        t3 = template_global(n=10)# sentence_length)
+
+        print(f'AR DB template reasonableness: ')
+
+        print(f't1: {t1}')
+        print(f't2: {t2}')
+        print(f't3: {t3}')
+
 
     def get_energy_dense(self, x): 
         '''
@@ -79,6 +134,16 @@ class TemplatesManualMH(nn.Module):
         return energy
 
     def forward(self, x): 
+        '''
+        Input the word feature vector
+        Output the weights vector
+        Softmax it to get linear combination (# TODO in templates_multihead_attention.py?)
+            Softmax := probability distribution over attn templates
+                if weights with sum to 1, output sum to 1
+                Saves us having to do softmax after summing over the templates
+        '''
+        
+        
         '''
         Parameters:
             x: Tensor (sequence length, batch size, dimension of token representation)
